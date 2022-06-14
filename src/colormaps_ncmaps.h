6335441cf6b9c03dcfca93a83e90e672bde6afa2
Thomas Lavergne <thomasl@pc5659.pc.met.no>
2022-06-14 22:00:41 +0200

    Manually editing file to bring the viridis colormap

--- src/colormaps_ncmaps.h
+++ src/colormaps_ncmaps.h
@@ -1,5 +1,7 @@
 // Colormaps provided by ncmaps
 #include "colormaps/colormaps_batlow.h"
+#include "colormaps/colormaps_viridis.h"
 
 #define INIT_NCMAPS_CMAPS \
-init_cmap_from_data( "batlow",  cmap_batlow  );
+init_cmap_from_data( "batlow",  cmap_batlow  ); \
+init_cmap_from_data( "viridis",  cmap_viridis  );
